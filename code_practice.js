3ff510ed6fbabf21d7e9d71e78b5dfdd1d34199e
SDgon <119998807+SDgon@users.noreply.github.com>
2022-12-16 23:56:16 +0900

    Add files via upload

--- code_practice.js
+++ code_practice.js
@@ -50,3 +50,112 @@ try {
 };
 //
 
+function book(){
+  var title = "js 책";
+};
+
+var point = function(one, two){
+  var total = one + two;
+  var bonus = total + 100;
+
+}
+
+function setValue(one, two){
+  var total = one + two;
+};
+
+setValue(10, 20);
+console.log(total);
+
+function getPoint(){
+  return 10 * 30;
+}
+var result = getPoint();
+console.log(result);
+
+/////////////////////////////////
+function getPoint() {
+  return
+  10 * 30;
+};
+var result = getPoint();
+console.log(result);
+//return문에서 return하므로 10 * 30을 수행x
+//출력 = undifind
+
+var book = {};
+book.title = "js책";
+console.log(book);
+//프로퍼티 추가
+//출력 = {title: js책}
+var book = {};
+book["title"] = "js책";
+console.log(book);
+//출력 = 위와 같음
+
+var obj = {book: "책"};
+console.log(obj.book);
+console.log(obj["sports"]);
+
+console.log(Number("123") + 500);
+//출력 623
+console.log(Number("ABC"));
+//출력 NaN
+
+var oneObj = new Number ("123");
+console.log(oneObj.valueOf());
+//////////////////////////
+var obj = new Number("123");
+console.log(obj.valueOf());
+// obj의 프리미티브 값을 반환, 출력 = 123
+
+
+var value = 20;
+console.log(20 === value.toString());    // 출력 = false,  string type 변환으로..
+console.log(value.toString(16));         // 출력 = 14,    20을 16진수로 변환때문
+
+var concat = 123 + 'acd' + "가나다라";
+console.log(concat);
+
+var value = String(123);
+console.log(value);          //출력 123
+console.log(typeof value);   //     string
+
+console.log(typeof ("" + 123)); //  string
+
+var value = "ABC";
+console.log(value.length);
+
+var value = "ABC";
+for (var k = 0; k < value.length; k++){
+  console.log(value[k]);
+}
+
+var value = "  abcd  ";
+console.log(value.length);     //출력 = 8
+
+console.log(value.trim().length); //앞뒤 공백 제거, 출력 = 4
+
+var value = "sports";
+console.log(value.charAt(1));    //출력 p
+console.log(value[1]);          //출력 p
+
+var value = "sports";
+console.log(value.charAt(12));   // 출력 = 없음(빈문자열)
+
+var value = "123123";
+console.log(value.indexOf(2));
+
+var value = "123123";
+console.log(value.lastIndexOf(1,-2));
+
+var result = "sports".concat("축구", 11);
+console.log(result);
+
+var obj = new String(123);
+console.log(obj.concat("ABC"));
+
+console.log(Object.create);      
+console.log(Object.prototype.create);
+
+
